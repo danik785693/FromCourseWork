d7400724b0a0cb34b6eb9a0f75ae97d6b5776cc5
danik785693 <danikt123123@gmail.com>
2023-05-02 13:30:10 +0300

    create controller flight

---
+++ src/controllers/flightController.ts
@@ -0,0 +1,54 @@
+/** @format */
+
+import { Request, Response } from "express";
+import { Flight } from "../models/models";
+
+class ConstructionMachineryCreate {
+  async create(req: Request, res: Response) {
+    try {
+      const { name, description, data, availabilitybilet } = req.body;
+      const createFlight = Flight.create({
+        name,
+        description,
+        data,
+        availabilitybilet,
+      });
+      return res.status(200).json(createFlight);
+    } catch (e) {
+      res.status(400).json(e);
+    }
+  }
+
+  async getAll(req: Request, res: Response) {
+    try {
+      const getallFlight = await Flight.findAll();
+      res.status(200).json(getallFlight);
+    } catch (e) {
+      res.status(400).json(e);
+    }
+  }
+
+  async getOne(req: Request, res: Response) {
+    try {
+      const id = req.query.id;
+      const getoneFlight = await Flight.findOne({
+        where: { id: id },
+      });
+      res.status(200).json(getoneFlight);
+    } catch (e) {
+      res.status(400).json(e);
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    try {
+      const id = req.query.id;
+      await Flight.destroy({ where: { id: id } });
+      res.status(200).json({ messege: "Рейс удалён" });
+    } catch (e) {
+      res.status(400).json(e);
+    }
+  }
+}
+
+export default new ConstructionMachineryCreate();
